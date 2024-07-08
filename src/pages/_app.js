32913407f4bb352de4ef1351cf47cc1b1e259e98
Bilawal Hassan <128214198+BilawalHassanF19csc17@users.noreply.github.com>
2024-07-09 03:06:14 +0500

    commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
 import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <>
+  <Head>
+    <title>Her&Hair</title>
+    <link rel="icon" href="/cropped-image-2.png" />
+  </Head>
+  <Component {...pageProps} />;
+  </>
 }
