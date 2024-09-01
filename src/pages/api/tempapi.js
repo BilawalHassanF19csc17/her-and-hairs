32771ecde5895f9b1d7cc0983f83a318e585495d
Bilawal Hassan <128214198+BilawalHassanF19csc17@users.noreply.github.com>
2024-09-01 17:05:57 +0500

    initial commit

--- src/pages/api/tempapi.js
+++ src/pages/api/tempapi.js
@@ -1,7 +1,32 @@
-export default async function handler(req, res) {
+import Cors from 'cors';
 
-    if(req.method === 'POST'){
-        const body =req.body;
+// Initialize the CORS middleware with specific settings
+const cors = Cors({
+  methods: ['POST', 'GET', 'HEAD'],
+  origin: 'https://herandhair.com',
+});
+
+// Helper function to run middleware
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+}
+
+export default async function handler(req, res) {
+  // Run the CORS middleware
+  await runMiddleware(req, res, cors);
+
+  if (req.method === 'POST') {
+    const body = await req.body;
+    console.log("Received data:", body);  // Log the received data
     res.status(200).json(body);
-    }
+  } else {
+    res.status(405).json({ message: "Method not allowed" });
+  }
 }
