1166ec99b709830dbf28b2e174301e1256e83df4
Bilawal Hassan <128214198+BilawalHassanF19csc17@users.noreply.github.com>
2024-09-01 16:33:52 +0500

    initial commit

--- src/pages/api/tempapi.js
+++ src/pages/api/tempapi.js
@@ -1,4 +1,7 @@
 export default async function handler(req, res) {
-    const body =req.body;
+
+    if(req.method === 'POST'){
+        const body =req.body;
     res.status(200).json(body);
+    }
 }
