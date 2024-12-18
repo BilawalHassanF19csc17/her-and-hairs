dff1f7dd04d74e6ea23b766504db8a029168c31b
Bilawal Hassan <128214198+BilawalHassanF19csc17@users.noreply.github.com>
2024-12-18 19:58:45 +0000

    initial commit

--- src/pages/api/customer/tracking.js
+++ src/pages/api/customer/tracking.js
@@ -21,6 +21,8 @@ const sendNotification = async (trackingID,customer,email) => {
         <p>You can track your order using the following tracking number <b>${trackingID}</b>. Simply visit this <a href="https://www.aramex.com/us/en/track/shipments">link</a> and enter your tracking number to check the status of your shipment.</p>
         <br>
         <p>If you have any query or need further assistance, feel free to reach out us at contact@herandhair.com. We are always happy to help!</p>
+        <p><b>Note to Customers: </b>
+        To ensure a smooth resolution for any claims regarding damaged or broken products, please make sure to record a video while unpacking your order. This will help us assist you promptly and effectively. Thank you for your understanding!</p>
         <br>
         <p style="font-style: italic;">Thank you for choosing Her & Hair</p>
         <p style="font-style: italic; margin: 0;">Best Regards,</p>
